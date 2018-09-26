2e6cd546caa186895fd51ec9b3ff3f7a6deb7da6
Joaquin Galvez Gomez <jgalvego@everis.com>
2018-09-26 17:22:17 +0200

    Proyecto Maven BMW con modulo maven BMW-app configurado para JAXRS, JPA y EJB

---
+++ BMW-app/src/main/java/com/everis/security/JAXRSConfiguration.java
@@ -0,0 +1,23 @@
+package com.everis.security;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ *
+ * @author airhacks.com
+ */
+@ApplicationPath("api/v1")
+public class JAXRSConfiguration extends Application {
+    @Override
+    public Map<String, Object> getProperties() {
+        Map<String, Object> props = new HashMap<>();
+        props.put("jersey.config.server.disableMoxyJson", true);
+        return props;
+    }
+}
